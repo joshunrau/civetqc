08aea9386a2ed09d6184f338a565871f0f8f04c1
joshunrau <joshuatunrau@protonmail.com>
2021-10-08 16:10:47 -0400

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qcivet',
+    version='0.1.0',
+    author='Joshua Unrau',
+    author_email='joshua.unrau@mail.mcgill.ca',
+    description='',
+    long_description='',
+    packages=find_packages(),
+    install_requires=[]
+)
