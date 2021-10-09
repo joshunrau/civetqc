f9bae509d76d72bcd6c7c93aa1e7443bd294b519
joshunrau <joshuatunrau@protonmail.com>
2021-10-09 11:55:41 -0400

    ignore /data

--- civetqc/__main__.py
+++ civetqc/__main__.py
@@ -2,8 +2,12 @@ from argparse import ArgumentParser
 from civetqc import Subject
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
+
+    parser = ArgumentParser(prog="civetqc")
     parser.add_argument("--prefix", type=str, nargs=1, required=True, help="file prefix")
     parser.add_argument("--id", type=str, nargs=1, required=True, help="subject id")
     args = parser.parse_args()
-    print(args)
+    
+    parser.add_argument()
+    test_subj = Subject(args.prefix[0], args.id[0])
+    print(test_subj)
