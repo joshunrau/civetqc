c36fed44d56f1b2447986608446bf38a8308b756
joshunrau <joshuatunrau@protonmail.com>
2021-10-14 14:47:13 -0400

    rename

--- civetpy/__main__.py
+++ civetpy/__main__.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 
 if __name__ == "__main__":
 
-    parser = ArgumentParser(prog="civetqc")
+    parser = ArgumentParser(prog="civetpy")
     parser.add_argument("--csv_path", type=str, nargs=1, required=True, help="path to csv file")
     parser.add_argument("--prefix", type=str, nargs=1, required=True, help="file prefix")
     parser.add_argument("--id", type=str, nargs=1, required=True, help="subject id")
