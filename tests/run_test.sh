a45aa4a479e2a10d1661da002f696a1cb29ec9f0
joshunrau <joshuatunrau@protonmail.com>
2021-10-25 21:28:06 -0400

    update test output dir

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -5,6 +5,6 @@ echo $(python --version) && echo $(which python)
 
 civet_output="/Users/joshua/Developer/civetqc/data/LAM/QC/civet_LAM_.csv"
 user_ratings="/Users/joshua/Developer/civetqc/data/LAM/user_ratings.csv"
-output_dir="/Users/joshua/Developer/civetqc/tests"
+output_dir="/Users/joshua/Developer/civetqc/data/Tests"
 civetqc "$civet_output" "$user_ratings" -o "$output_dir"
 
