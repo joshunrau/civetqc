64d6e68b95da9b70da9de28f0ab9c453132805b7
joshunrau <joshuatunrau@protonmail.com>
2021-10-26 01:59:19 -0400

    add run_model

---
+++ civetqc/run_model.py
@@ -0,0 +1,8 @@
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+# split dataset into testing and training
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+# instantiate the KNN class
+clf = KNeighborsClassifier(n_neighbors=3)
