e95bdaffcf50d3ccf4b7f257ce46dcdee0ecd790
joshunrau <joshuatunrau@protonmail.com>
2021-10-26 21:39:10 -0400

    add usage

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # CivetQC
 
 This repository is currently a work in progress. One major goal is to provide a command line tool to easily estimate quality control ratings for a large number of CIVET outputs by generalizing from a much smaller number of known examples. 
+
+## Usage
+
+    usage: civetqc [-h] [--output_dir OUTPUT_DIR] civet_output user_ratings
+
+    positional arguments:
+    civet_output          Path to CSV file outputted by CIVET for QC
+    user_ratings          Path to CSV file containing user QC ratings
+
+    optional arguments:
+    -h, --help            show this help message and exit
+    --output_dir OUTPUT_DIR, -o OUTPUT_DIR
+                            Path to directory where results will be outputted
--- civetqc/parse_args.py
+++ civetqc/parse_args.py
@@ -1,14 +1,9 @@
 import argparse
 
-
 def parse_args(args) -> argparse.Namespace:
     """ parse arguments given at command line """
     parser = argparse.ArgumentParser(prog="civetqc")
-    parser.add_argument("civet_output", type=str, nargs=1, 
-                        help="The path to the quality control csv file outputed by CIVET")
-    parser.add_argument("user_ratings", type=str, nargs=1, 
-                        help="The path to the csv file containing user quality control ratings")
-    parser.add_argument("--output_dir", "-o", type=str, required=False, nargs=1, 
-                        help="The path to directory where files should be outputted")
+    parser.add_argument("civet_output", type=str, nargs=1, help="Path to CSV file outputted by CIVET for QC")
+    parser.add_argument("user_ratings", type=str, nargs=1, help="Path to CSV file containing user QC ratings")
+    parser.add_argument("--output_dir", "-o", type=str, required=False, nargs=1, help="Path to directory where results will be outputted")
     return parser.parse_args(args)
-    
