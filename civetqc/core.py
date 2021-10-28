eb6bb84df6fa1c3cc3c9f42f5ae9d06d67f120b5
joshunrau <joshuatunrau@protonmail.com>
2021-10-28 15:15:46 -0400

    add docs

--- civetqc/core.py
+++ civetqc/core.py
@@ -1,22 +1,10 @@
-import numpy as np
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 import typing
 
 
-class InvalidFileTypeError(Exception):
-    pass
-
-class VariableNotFoundError(Exception):
-    pass
-
-class DuplicateIdentifierError(Exception):
-    pass
-
-
-class Dataset:
+class BaseData:
     """ 
     Class with methods to setup data from csv file for analysis
     
@@ -31,49 +19,66 @@ class Dataset:
     data : pd.DataFrame
         dataframe imported from path_csv
     
+    Methods
+    -------
+    vars_in_cols(self, list_vars: list)
+        returns whether all strings in list_vars are in data.columns
+
+    write_data(self, output_dir: str, filename: str)
+        writes data to filename in output_dir
+
+    col_to_numeric(self, var: str)
+        converts column in data to numeric, coercing non-numeric values to NaN
+
+    print_data(self, var: typing.Optional[str]=None)
+        prints data with all rows and columns, or data[var] with all rows
+    
+    is_unique(s: pd.Series)
+        returns whether all values in series are unique
+
+    is_csv(filepath)
+        returns whether filepath ends with .csv
+
     """
 
     idvar = "ID"
 
     def __init__(self, path_csv: str, required_vars: list) -> None:
-        
-        # Ensure file exists and has .csv extension
+        """
+        Parameters
+        ----------
+        path_csv: str
+            path to csv file to import
+        required_vars: list
+            list of variables required to be in csv file
+        """
+
         if not os.path.isfile(path_csv):
             raise FileNotFoundError(f"File '{path_csv}' does not exist")
         if not self.is_csv(path_csv):
-            raise InvalidFileTypeError(f"File '{path_csv}' must be in csv format")
-        
-        # Read csv file and ensure all required variables are present
+            raise ValueError(f"File '{path_csv}' must be in csv format")
+
         self.data = pd.read_csv(path_csv)
+
         if not self.vars_in_cols(required_vars):
-            raise VariableNotFoundError(f"Required field not found in file {path_csv}")
-        
-        # Ensure all values for ID variable are unique
+            raise RuntimeError(f"Required field not found in file {path_csv}")
         if not self.is_unique(self.data[self.idvar]):
-            raise DuplicateIdentifierError(f"Non-unique values for ID variable in file {path_csv}")
-        
-    def vars_in_cols(self, list_vars) -> bool:
+            raise RuntimeError(f"Non-unique values for ID variable in file {path_csv}")
+    
+    def vars_in_cols(self, list_vars: list) -> bool:
         for var in list_vars:
             if var not in self.data.columns:
                 return False
         return True
     
-    def write_data(self, output_dir: str, dir_name: str = "civetqc", filename: str = "df.csv") -> None:
-        """ write csv file for documentation """
+    def write_data(self, output_dir: str, filename: str) -> None:
+        assert os.path.isdir(output_dir)  # should check for this in case
+        self.data.to_csv(path_or_buf = os.path.join(output_dir, filename))
 
-        if not os.path.isdir(output_dir):
-            raise NotADirectoryError(f"output directory {output_dir} does not exist")
-
-        if not os.path.isdir(os.path.join(output_dir, dir_name)):
-            os.mkdir(os.path.join(output_dir, dir_name))
-
-        self.data.to_csv(path_or_buf = os.path.join(output_dir, dir_name, filename))
-    
     def col_to_numeric(self, var: str) -> None:
         self.data[var] = self.data[var].apply(pd.to_numeric, errors='coerce')
     
     def print_data(self, var: typing.Optional[str]=None) -> None:
-        """ print all rows and columns in pandas dataframe, or all rows in column """
         if var is None:
             with pd.option_context('display.max_rows', None, 'display.max_columns', None):
                 print(self.data)
@@ -83,15 +88,26 @@ class Dataset:
 
     @staticmethod
     def is_unique(s: pd.Series) -> bool:
-        """ return whether all values in series are unique """
         return len(s.unique()) == len(s)
-        
+    
     @staticmethod
-    def is_csv(filepath) -> bool:
+    def is_csv(filepath: str) -> bool:
         return filepath.split(".")[-1] == "csv"
 
 
-class CivetOutput(Dataset):
+class CivetData(BaseData):
+    """ 
+    Subclass of BaseData used to import CIVET QC output file
+    
+    ...
+
+    Attributes
+    ----------
+
+    civet_vars : list
+        the features outputted by CIVET
+
+    """
     civet_vars = [
         "MASK_ERROR", "WM_PERCENT", "GM_PERCENT", "CSF_PERCENT", "SC_PERCENT", 
         "BRAIN_VOL", "CEREBRUM_VOL", "CORTICAL_GM", "WHITE_VOL", "SUBGM_VOL", 
@@ -104,32 +120,69 @@ class CivetOutput(Dataset):
         super().__init__(path_csv, [self.idvar] + self.civet_vars)
         
 
-class UserRatings(Dataset):
+class UserData(BaseData):
+    """ 
+    Subclass of BaseData used to import user QC ratings
+    
+    ...
+
+    Attributes
+    ----------
+
+    qcvar : str
+        the name of the column containing user QC ratings
+
+    """
     qcvar = "QC"
     def __init__(self, path_csv: str) -> None:
         super().__init__(path_csv, [self.idvar, self.qcvar])
 
 
-class CivetData(CivetOutput, UserRatings):
-    def __init__(self, civet_output: CivetOutput, user_ratings: UserRatings, drop_na: bool = False) -> None:
-        self.data = pd.merge(civet_output.data, user_ratings.data, on=self.idvar)
-        if drop_na:
-            self.data = self.data.dropna() 
+class Dataset(CivetData, UserData):
+    """ 
+    Class used to merge and organize data for analysis
+    
+    ...
+
+    Attributes
+    ----------
+
+    data : pd.DataFrame
+        merged dataframe created from CivetData and UserData objects when instantiated
+    
+    features : pd.DataFrame
+        subset of data containing features from the CIVET QC output
+    
+    target: pd.Series
+        subset of data containing the known QC ratings by the user
+
+    feat_train
+        training set of features (75%)
+    
+    feat_test
+        testing set of features (25%)
+    
+    targ_train
+        training set of target (75%)
+
+    targ_test
+        testing set of target (25%)
+
+    """
+
+    def __init__(self, civet_csv: str, user_csv: str) -> None:
+        """
+        Parameters
+        ----------
+        civet_csv: str
+            path to the csv file outputted by CIVET
+        user_csv: str
+            path to the csv file containing the user's QC ratings
+        """
+        civet_output = CivetData(civet_csv)
+        user_ratings = UserData(user_csv)
+        self.data = pd.merge(civet_output.data, user_ratings.data, on=self.idvar).dropna()
         self.features = self.data[self.civet_vars]
         self.target = self.data[self.qcvar]
         self.feat_train, self.feat_test, self.targ_train, self.targ_test = train_test_split(
             self.features, self.target, random_state=0)
-    
-    def test_knn(self, r: typing.Union[int, range]) -> None:
-        if type(r) == int:
-            r = range(r, r+1)
-        for i in r:
-            knn = KNeighborsClassifier(n_neighbors=i)
-            knn.fit(self.feat_train, self.targ_train)
-            targ_pred = knn.predict(self.feat_test)
-            print(sum(self.data["QC"] == '2')/len(self.data))
-            print(sum(self.data["QC"] == '1')/len(self.data))
-            print(sum(self.data["QC"] == '0')/len(self.data))
-            print(targ_pred)
-            print(list(self.targ_test))
-            print("Test set score: {:.2f}".format(np.mean(targ_pred == self.targ_test)))
--- civetqc/tests.py
+++ civetqc/tests.py
@@ -1,5 +1,5 @@
 import unittest
-from .core import CivetOutput, UserRatings, CivetData
+from .core import Dataset
 
 
 PATH_CIVET_OUTPUT = "/Users/joshua/Developer/civetqc/data/LAM/QC/civet_LAM_.csv"
@@ -7,7 +7,7 @@ PATH_USER_RATINGS = "/Users/joshua/Developer/civetqc/data/LAM/user_ratings.csv"
 OUTPUT_DIR = "/Users/joshua/Developer/civetqc/data/Tests"
 
 
-class TestCore(unittest.TestCase):
+class TestDataset(unittest.TestCase):
     def test_core(self):
-        dataset = CivetData(CivetOutput(PATH_CIVET_OUTPUT), UserRatings(PATH_USER_RATINGS), drop_na=True)
+        dataset = Dataset(PATH_CIVET_OUTPUT, PATH_USER_RATINGS)
         self.assertEquals(dataset.data.shape, (333, 44))
