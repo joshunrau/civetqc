7c1acd030187ffe05965b29eb13cc48b53a69c18
joshunrau <joshuatunrau@protonmail.com>
2021-11-05 18:44:22 -0400

    somewhat working

--- civetqc/dataset.py
+++ civetqc/dataset.py
@@ -1,7 +1,25 @@
 import os
+from typing import Union
 import numpy as np
 import pandas as pd
+from scipy.sparse import data
 from sklearn.model_selection import train_test_split
+
+
+STUDIES_DIR = "/Users/joshua/Developer/civetqc/data/studies"
+
+
+CIVET_FILE_PATHS = {
+    "FEP": os.path.join(STUDIES_DIR, "FEP", "FEP_civet_data.csv"),
+    "LAM": os.path.join(STUDIES_DIR, "LAM", "LAM_civet_data.csv"),
+    "TOPSY": os.path.join(STUDIES_DIR, "TOPSY", "TOPSY_civet_data.csv")
+}
+
+QC_FILE_PATHS = {
+    "FEP": os.path.join(STUDIES_DIR, "FEP", "FEP_QC.csv"),
+    "LAM": os.path.join(STUDIES_DIR, "LAM", "LAM_QC.csv"),
+    "TOPSY": None
+}
 
 
 class VariableNotFoundError(Exception):
@@ -12,13 +30,6 @@ class VariableNotFoundError(Exception):
 class DuplicateIdentifierError(Exception):
     """ raised when a value for the ID variable appears more than once """
     pass
-
-
-class DataPartition:
-    """ container for test and training partitions of target or features """
-    def __init__(self, train: np.ndarray, test: np.ndarray) -> None:
-        assert isinstance(train, np.ndarray) and isinstance(test, np.ndarray)
-        self.train, self.test = train, test
 
 
 class StudyData:
@@ -40,12 +51,10 @@ class StudyData:
         a list of variable names required to be in the CIVET output file
     required_user_vars : list
         a list of variable names required to be in the user ratings file
+    required_all_vars : list
+        a list of variable names that must be in self.df
     df : pd.DataFrame
         merged dataframe created from CIVET output and user ratings
-    features : DataPartition
-        contains testing and training sets of features
-    target : DataPartition
-        contains testing and training sets of the target
     
     Operators
     ---------
@@ -54,18 +63,11 @@ class StudyData:
         Returns True if other is an object of self.__class__ and idvar,
         qcvar, and df are equal in both objects
     
-    __str__(self)
-        Returns the number of total observations and the target counts
-        for both the testing and training sets
-    
     Instance Methods
     ----------------
     
     all_in_range(self, var: str, r: int)
         returns whether all values in self.df[var] are in range(r)
-
-    format_class_counts(self, d: dict)
-        returns a formated string of the class counts in the test and training sets
 
     Static Methods
     --------------
@@ -74,9 +76,7 @@ class StudyData:
         raises a VariableNotFoundError if all strings in list_vars are not in df.columns
     is_unique(s: pd.Series, var_name: str, filename: str)
         raises a DuplicateIdentifierError if there are any non-unique values in s
-    get_array_counts(arr: np.ndarray)
-        returns a dict with the number of occurrences of each value in arr
-
+    
     """
 
     idvar = "ID"
@@ -91,6 +91,7 @@ class StudyData:
     ]
     required_civet_vars = [idvar] + civet_feature_names
     required_user_vars = [idvar, qcvar]
+    required_all_vars = [idvar, qcvar] + civet_feature_names
 
     def __init__(self, civet_csv: str, user_csv: str, cutoff_value: int = 1) -> None:
         """
@@ -114,6 +115,7 @@ class StudyData:
         self.is_unique(user_ratings[self.idvar], self.idvar, user_csv)
     
         self.df = pd.merge(civet_data, user_ratings, on=self.idvar).dropna()
+        self.df = self.df[[self.idvar, self.qcvar] + self.civet_feature_names]
         self.df[self.qcvar] = self.df[self.qcvar].apply(pd.to_numeric, errors='coerce')
 
         if not all(self.df[self.qcvar] >= 0):
@@ -121,46 +123,25 @@ class StudyData:
         
         self.df[self.qcvar] = np.where(self.df[self.qcvar] < cutoff_value, 0, 1)
         assert self.all_in_range(self.qcvar, 2)
-
-        features_array = self.df[self.civet_feature_names].to_numpy()
-        target_array = self.df[self.qcvar].to_numpy()
-        x_train, x_test, y_train, y_test = train_test_split(features_array, target_array, random_state=0)
-        self.features = DataPartition(x_train, x_test)
-        self.target = DataPartition(y_train, y_test)
     
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, self.__class__):
             return False
         return self.idvar == other.idvar and self.qcvar == other.qcvar and self.df.equals(other.df)
-    
-    def __str__(self) -> str:
-        dataset_header = "DATASET"
-        horizontal_line = "----------------------------------------------------------------------"
-        num_observ =  f"Number of Observations: {len(self.df)}"
-        target_train = f"Target Train:\n{self.format_class_counts(self.get_array_counts(self.target.train))}"
-        target_test = f"Target Test:\n{self.format_class_counts(self.get_array_counts(self.target.test))}"
-        return "\n".join([dataset_header, horizontal_line, num_observ, target_train, target_test])
 
     def all_in_range(self, var: str, r: int) -> bool:
         for value in self.df[var]:
             if value not in range(r):
                 return False
         return True
-    
-    def format_class_counts(self, d: dict) -> str:
-        list_strings = []
-        sum_classes = 0
-        for key in d:
-            sum_classes += d[key]
-        for key in d:
-            list_strings.append(f"{key}: {d[key]} ({round(d[key]/sum_classes*100, 1)}%)")
-        return "\n".join(list_strings)
 
     @staticmethod
-    def vars_in_cols(df: pd.DataFrame, list_vars: list, filename: str) -> None:
+    def vars_in_cols(df: pd.DataFrame, list_vars: list, filename: Union[str, None] = None) -> None:
         assert isinstance(df, pd.DataFrame) and isinstance(list_vars, list)
         for var in list_vars:
             if var not in df.columns:
+                if filename is None:
+                    raise VariableNotFoundError(f"Required variable {var} not found in columns {df.columns}")
                 raise VariableNotFoundError(f"Required variable {var} not found in file {filename}")
     
     @staticmethod
@@ -177,6 +158,80 @@ class StudyData:
             raise AssertionError
         elif len(duplicated_values) != 0:
             raise DuplicateIdentifierError(f"Non-unique values {duplicated_values} for {var_name} in file {filename}")
+        
+            
+class DataPartition:
+    """ container for test and training partitions of target or features """
+    def __init__(self, train: np.ndarray, test: np.ndarray) -> None:
+        assert isinstance(train, np.ndarray) and isinstance(test, np.ndarray)
+        self.train, self.test = train, test
+
+
+class Dataset(StudyData):
+    """ 
+    Class including the information from all datasets 
+
+    ...
+
+    Attributes
+    ----------
+
+    df : pd.DataFrame
+        merged dataframe containing all datasets
+    features : DataPartition
+        contains testing and training sets of features
+    target : DataPartition
+        contains testing and training sets of the target
+    
+    Operators
+    ---------
+
+    __str__(self)
+        Returns the number of total observations and the target counts
+        for both the testing and training sets
+    
+    Instance Methods
+    ----------------
+
+    format_class_counts(self, d: dict)
+        returns a formated string of the class counts in the test and training sets
+    
+    Static Methods
+    --------------
+
+    get_array_counts(arr: np.ndarray)
+        returns a dict with the number of occurrences of each value in arr
+
+    """
+    def __init__(self, *args) -> None:
+        """ takes objects of type StudyData as args """
+        assert all([isinstance(x, StudyData) for x in args])
+        self.df = args[0].df
+        for dataset in args[1:]:
+            self.vars_in_cols(dataset.df, self.required_all_vars)
+            self.df.merge(dataset.df, on=self.required_all_vars)
+        features_array = self.df[self.civet_feature_names].to_numpy()
+        target_array = self.df[self.qcvar].to_numpy()
+        x_train, x_test, y_train, y_test = train_test_split(features_array, target_array, random_state=0)
+        self.features = DataPartition(x_train, x_test)
+        self.target = DataPartition(y_train, y_test)
+    
+    def __str__(self) -> str:
+        dataset_header = "DATASET"
+        horizontal_line = "----------------------------------------------------------------------"
+        num_observ =  f"Number of Observations: {len(self.df)}"
+        target_train = f"Target Train:\n{self.format_class_counts(self.get_array_counts(self.target.train))}"
+        target_test = f"Target Test:\n{self.format_class_counts(self.get_array_counts(self.target.test))}"
+        return "\n".join([dataset_header, horizontal_line, num_observ, target_train, target_test])
+    
+    def format_class_counts(self, d: dict) -> str:
+        list_strings = []
+        sum_classes = 0
+        for key in d:
+            sum_classes += d[key]
+        for key in d:
+            list_strings.append(f"{key}: {d[key]} ({round(d[key]/sum_classes*100, 1)}%)")
+        return "\n".join(list_strings)
     
     @staticmethod
     def get_array_counts(arr: np.ndarray) -> dict:
@@ -186,11 +241,14 @@ class StudyData:
         for i in range(len(counts_array)):
             counts[counts_array[i, 0]] = counts_array[i, 1]
         return counts
-        
-            
-class Dataset(StudyData):
-    """ class including the information from all datasets """
 
-    def __init__(self, *args) -> None:
-        for dataset in args:
-            print(dataset)
+
+def main():
+    fep_dataset = StudyData(CIVET_FILE_PATHS["FEP"], QC_FILE_PATHS["FEP"])
+    lam_dataset = StudyData(CIVET_FILE_PATHS["LAM"], QC_FILE_PATHS["LAM"])
+    dataset = Dataset(fep_dataset, lam_dataset)
+    print(dataset)
+
+
+if __name__ == "__main__":
+    main()
