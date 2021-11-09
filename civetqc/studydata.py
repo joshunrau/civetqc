a5a0e2ea90c5f4a6510f82e034b6f8615042ea06
joshunrau <joshuatunrau@protonmail.com>
2021-11-09 17:30:36 -0500

    cleanup

--- civetqc/studydata.py
+++ civetqc/studydata.py
@@ -2,21 +2,13 @@ import os
 from typing import Union
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
-"""
-
-TO DO:
-    Manage floating point values for QC ratings
-    iRELATE: still waiting on whether QC was done
-    NUSDAST: extract CIVET outputs from /data/lepage/NUSDAST/data/processed/civet2.1.0_bpipe_Niagara
-    
-"""
 
 STUDIES_DIR = "/Users/joshua/Developer/civetqc/data/studies"
+OUTPUT_FILE = os.path.join(STUDIES_DIR, "master_dataset.csv")
 
 
-FILEPATHS = {
+STUDY_FILEPATHS = {
     "FEP": (
         os.path.join(STUDIES_DIR, "FEP", "FEP_civet_data.csv"), 
         os.path.join(STUDIES_DIR, "FEP", "FEP_QC.csv")
@@ -61,19 +53,9 @@ class DataFrameMergerError(ValueError):
     pass
 
 
-class DataPartition:
-    """ container for test and training partitions of target or features """
-    def __init__(self, train: np.ndarray, test: np.ndarray) -> None:
-        assert isinstance(train, np.ndarray) and isinstance(test, np.ndarray)
-        self.train, self.test = train, test
-
-
 class Dataset:
     """ 
-    This class contains the essential attributes and methods used to import and organize 
-    the data for the model. In its basic form, the Dataset class includes data from one 
-    CIVET output and one QC ratings file. The class method 'master_dataset' can be used
-    to create an object of type Dataset containing data from multiple studies.
+    This class contains data from one CIVET output and one QC ratings file.
     
     ...
 
@@ -93,35 +75,17 @@ class Dataset:
         List of variable names that must be in self.df
     df : pd.DataFrame
         Merged dataframe created from CIVET output and user ratings
-    features : DataPartition
-        Contains testing and training sets of features
-    target : DataPartition
-        Contains testing and training sets of the target
-
-    Operators
-    ---------
-    __eq__(self, other: object)
-        Returns True if other is an object of self.__class__ and idvar,
-        qcvar, and df are equal in both objects
-    __str__(self)
-        Returns the number of total observations and the target counts
-        for both the testing and training sets
 
     Instance Methods
     ----------------
     all_in_range(self, var: str, r: int)
         Returns whether all values in self.df[var] are in range(r)
-
+    
     Static Methods
     --------------
-    format_class_counts(d: dict)
-        Returns a formatted string of the class counts in the test and training sets
     vars_in_cols(df: pd.DataFrame, list_vars: list, filename: str)
         Raises a VariableNotFoundError if all strings in list_vars are not in df.columns
-    is_unique(s: pd.Series, var_name: str, filename: str)
-        Raises a DuplicateIdentifierError if there are any non-unique values in s
-    get_array_counts(arr: np.ndarray)
-        Returns a dict with the number of occurrences of each value in arr
+    
     """
 
     idvar = "ID"
@@ -138,31 +102,35 @@ class Dataset:
     required_user_vars = [idvar, qcvar]
     required_all_vars = [idvar, qcvar] + civet_feature_names
 
-    def __init__(self, civet_csv: str, user_csv: str, cutoff_value: int = 1) -> None:
+    def __init__(self, civet_csv: str, qc_csv: str, cutoff_value: int = 1) -> None:
         """
         Parameters
         ----------
         civet_csv: str
             path to the csv file outputted by CIVET
-        user_csv: str
+        qc_csv: str
             path to the csv file containing the user's QC ratings
         cutoff_value: int
             the cutoff value for a valid scan
         """
 
-        civet_data = pd.read_csv(civet_csv)
-        user_ratings = pd.read_csv(user_csv)
+        self.civet_data = pd.read_csv(civet_csv)
+        self.qc_data = pd.read_csv(qc_csv)
 
-        self.vars_in_cols(civet_data, self.required_civet_vars, civet_csv)
-        self.vars_in_cols(user_ratings, self.required_user_vars, user_csv)
+        self.vars_in_cols(self.civet_data, self.required_civet_vars, civet_csv)
+        self.vars_in_cols(self.qc_data, self.required_user_vars, qc_csv)
 
-        self.is_unique(civet_data[self.idvar], self.idvar, civet_csv)
-        self.is_unique(user_ratings[self.idvar], self.idvar, user_csv)
+        # Check that all values for ID variable are unique
+        if not self.civet_data[self.idvar].unique():
+            raise DuplicateIdentifierError(f"Non-unique values for ID variable in file {civet_csv}")
+        if not self.qc_data[self.idvar].unique():
+            raise DuplicateIdentifierError(f"Non-unique values for ID variable in file {qc_csv}")
 
+        # ValueError may be raised if the data types are different
         try:
-            self.df = pd.merge(civet_data, user_ratings, on=self.idvar).dropna()
+            self.df = pd.merge(self.civet_data, self.qc_data, on=self.idvar).dropna()
         except ValueError as err:
-            raise DataFrameMergerError(f"Error merging dataframes from files '{civet_csv}' and '{user_csv}, {err}")
+            raise DataFrameMergerError(f"Error merging dataframes from files '{civet_csv}' and '{qc_csv}, {err}")
         
         self.df = self.df[[self.idvar, self.qcvar] + self.civet_feature_names]
         self.df[self.qcvar] = self.df[self.qcvar].apply(pd.to_numeric, errors='coerce')
@@ -176,40 +144,11 @@ class Dataset:
         self.df[self.qcvar] = np.where(self.df[self.qcvar] < cutoff_value, 0, 1)
         assert self.all_in_range(self.qcvar, 2)
 
-        features_array = self.df[self.civet_feature_names].to_numpy()
-        target_array = self.df[self.qcvar].to_numpy()
-        x_train, x_test, y_train, y_test = train_test_split(features_array, target_array, random_state=1)
-        self.features = DataPartition(x_train, x_test)
-        self.target = DataPartition(y_train, y_test)
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, self.__class__):
-            return False
-        return self.idvar == other.idvar and self.qcvar == other.qcvar and self.df.equals(other.df)
-    
-    def __str__(self) -> str:
-        dataset_header = "DATASET"
-        horizontal_line = "----------------------------------------------------------------------"
-        observations = f"Number of Observations: {len(self.df)}"
-        target_train = f"Target Train:\n{self.format_class_counts(self.get_array_counts(self.target.train))}"
-        target_test = f"Target Test:\n{self.format_class_counts(self.get_array_counts(self.target.test))}"
-        return "\n".join([dataset_header, horizontal_line, observations, target_train, target_test])
-
     def all_in_range(self, var: str, r: int) -> bool:
         for value in self.df[var]:
             if value not in range(r):
                 return False
         return True
-    
-    @staticmethod
-    def format_class_counts(d: dict) -> str:
-        list_strings = []
-        sum_classes = 0
-        for key in d:
-            sum_classes += d[key]
-        for key in d:
-            list_strings.append(f"{key}: {d[key]} ({round(d[key]/sum_classes*100, 1)}%)")
-        return "\n".join(list_strings)
 
     @staticmethod
     def vars_in_cols(df: pd.DataFrame, list_vars: list, filename: Union[str, None] = None) -> None:
@@ -219,36 +158,11 @@ class Dataset:
                 if filename is None:
                     raise VariableNotFoundError(f"Required variable {var} not found in columns {df.columns}")
                 raise VariableNotFoundError(f"Required variable {var} not found in file {filename}")
-    
-    @staticmethod
-    def is_unique(s: pd.Series, var_name: str, filename: str) -> None:
-        assert isinstance(s, pd.Series)
-        unique_values = []
-        duplicated_values = []
-        for value in s:
-            if value not in unique_values:
-                unique_values.append(value)
-            else:
-                duplicated_values.append(value)
-        if len(s.unique()) != len(unique_values):
-            raise AssertionError
-        elif len(duplicated_values) != 0:
-            raise DuplicateIdentifierError(f"Non-unique values {duplicated_values} for {var_name} in file {filename}")
-    
-    @staticmethod
-    def get_array_counts(arr: np.ndarray) -> dict:
-        assert isinstance(arr, np.ndarray) and arr.ndim == 1
-        counts_array = np.array(np.unique(arr, return_counts=True)).T
-        counts = {}
-        for i in range(len(counts_array)):
-            counts[counts_array[i, 0]] = counts_array[i, 1]
-        return counts
 
 
 class MasterDataset(Dataset):
 
-    def __init__(self, filepaths: dict = FILEPATHS, cutoff_value: int = 1) -> None:
-
+    def __init__(self, filepaths: dict, cutoff_value: int) -> None:
         if not isinstance(filepaths, dict):
             raise TypeError(f"Expected argument of {dict} but received {type(filepaths)}")
         for key in filepaths:
@@ -266,14 +180,13 @@ class MasterDataset(Dataset):
                 self.df = pd.concat([self.df, dataset_tmp.df])
             except AttributeError:
                 super().__init__(filepaths[key][0], filepaths[key][1], cutoff_value)
-        
-        features_array = self.df[self.civet_feature_names].to_numpy()
-        target_array = self.df[self.qcvar].to_numpy()
-        x_train, x_test, y_train, y_test = train_test_split(features_array, target_array, random_state=1)
-        self.features = DataPartition(x_train, x_test)
-        self.target = DataPartition(y_train, y_test)
-
         assert self.df[self.idvar].is_unique
         self.df[self.idvar] = list(range(1, len(self.df[self.idvar]) + 1))
+    
+    def save_df(self, output_path):
+        self.df.to_csv(output_path, index=False)
 
 
+def main():
+    master_dataset = MasterDataset(filepaths = STUDY_FILEPATHS, cutoff_value=1)
+    master_dataset.save_df(os.path.join(STUDIES_DIR, "master_dataset.csv"))
