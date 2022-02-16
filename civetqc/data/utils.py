7634c165101ba2292c5a19fb74aa97bfbd866226
joshunrau <joshua@joshuaunrau.com>
2022-02-15 23:49:58 -0500

    update utils

--- civetqc/data/utils.py
+++ civetqc/data/utils.py
@@ -1,3 +1,4 @@
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import ndimage as ndi
@@ -6,9 +7,12 @@ from skimage.exposure import rescale_intensity
 from skimage.filters import sobel, prewitt_h, prewitt_v
 from skimage.feature import canny
 from skimage.io import imread, imshow
-from skimage.measure import label, regionprops
+from skimage.measure import label, regionprops, find_contours
 from skimage.segmentation import clear_border, watershed
 
+def load(filepath):
+    """ return image without alpha channel """
+    return imread(filepath)[:,:,:3]
 
 def show(img):
     plt.figure(figsize = (30, 10), dpi = 150)
@@ -40,8 +44,17 @@ def split_verify_images(img):
 
 def get_color_maps(img):
     rgb_maps = [np.copy(img), np.copy(img), np.copy(img)]
-    for i in range(3):
-        for j in range(3):
-            if i != j:
-                rgb_maps[i][:,:,j] = 0
+    rgb_maps[0][:,:,1], rgb_maps[0][:,:,2] = 0, 0
+    rgb_maps[1][:,:,0], rgb_maps[1][:,:,2] = 0, 0
+    rgb_maps[2][:,:,0], rgb_maps[2][:,:,1] = 0, 0
     return [rescale_intensity(rgb2gray(color)) for color in rgb_maps]
+
+def from_color_maps(red, green, blue):
+    return np.stack((red, green, blue), axis = -1)
+
+def get_pure_color_maps(img):
+    red, green, blue = get_color_maps(img)
+    pure_red = np.logical_and.reduce([red == 1, green == 0,  blue == 0])
+    pure_green = np.logical_and.reduce([red == 0, green == 1, blue == 0])
+    pure_blue = np.logical_and.reduce([red == 0, green == 0, blue == 1])
+    return pure_red, pure_green, pure_blue
