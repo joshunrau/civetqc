7a3113dbd15c33fe2858985aca95a7d7d2972a76
joshunrau <joshua@joshuaunrau.com>
2022-02-21 17:58:14 -0500

    add new model

--- civetqc/data/study.py
+++ civetqc/data/study.py
@@ -1,8 +1,6 @@
-import multiprocessing as mp
 import numpy as np
 import os
 import pandas as pd
-import time
 
 from . import RAW_DATA_DIRECTORY, ALL_FILE_SUFFIXES
 from .images import AnglesImage, AtlasImage, GradientImage, LaplaceImage, SurfSurfImage
@@ -116,44 +114,3 @@ class Study:
         df = df.merge(self.get_image_features(), on="ID")
         df = df.dropna(axis=1, how="all")
         queue.put(df)
-
-
-def create_dataset():
-    
-    start = time.perf_counter()
-
-    studies = [
-        Study(name="FEP", id_prefix="FEP"),
-        Study(name="INSIGHT", id_prefix="Insight"),
-        Study(name="LAM", id_prefix="LAM"),
-        Study(name="NUSDAST"),
-        Study(name="TOPSY", id_len=3, includes="V1_gradient_n4_anlm0.5r")
-    ]
-
-    queue = mp.Queue()
-
-    processes = []
-    results = []
-
-    for study in studies:
-        process = mp.Process(target=study.get_data, args=(queue,))
-        processes.append(process)
-        process.start()
-    
-    for process in processes:
-        df = queue.get() # will block
-        results.append(df)
-
-    for process in processes:
-        process.join()
-    
-    for df in results:
-        try:
-            dataset = pd.concat([dataset, df], ignore_index=True)
-        except NameError:
-            dataset = df
-    
-    stop = time.perf_counter()
-    print(f"Created dataset in {stop - start} seconds")
-    
-    return dataset.dropna(axis=1, how="any")
--- civetqc/model.py
+++ civetqc/model.py
@@ -1,25 +1,18 @@
 from sklearn.metrics import classification_report, fbeta_score, make_scorer
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
+from .data.dataset import Dataset
 
 class Model:
     
     f2_scorer = make_scorer(fbeta_score, beta=2)
+    path_saved_data = "/Users/joshua/Developer/civetqc/data/processed/civetqc_data_Feb20.csv"
     
-    def __init__(self, clf, df, **kwargs):
+    def __init__(self, clf, **kwargs):
         
-        self.df = df
-        
-        self.features = df.drop(["ID", "QC"], axis=1).to_numpy()
-        self.target = df["QC"].to_numpy()
-        
-        x_train, x_test, y_train, y_test = train_test_split(
-            self.features, self.target, test_size=0.33, random_state=0)
-        self.train, self.test = {}, {}
-        self.train["Features"], self.test["Features"] = x_train, x_test
-        self.train["Target"], self.test["Target"] = y_train, y_test
+        self.data = Dataset.from_file(self.path_saved_data)
         
         self.pipe = Pipeline([
             ("scaler", StandardScaler()),
@@ -27,12 +20,21 @@ class Model:
         ])
         
         self.grid = GridSearchCV(self.pipe, param_grid=kwargs, n_jobs=-1, scoring=self.f2_scorer)
-        self.grid.fit(self.train["Features"], self.train["Target"])
+        self.grid.fit(self.data.train["Features"], self.data.train["Target"])
+        self.predicted = self.grid.predict(self.data.test["Features"])
         
     def __str__(self):
-        return classification_report(self.test['Target'], self.predicted)
+        return classification_report(self.data.test['Target'], self.predicted)
     
     @property
-    def predicted(self):
-        return self.grid.predict(self.test["Features"])
-
+    def probabilities(self):
+        try:
+            return self.grid.predict_proba(self.data.test["Features"])
+        except AttributeError:
+            return None
+    
+    def decision_function(self):
+        try:
+            return self.grid.decision_function(self.data.test["Features"])
+        except AttributeError:
+            return None
